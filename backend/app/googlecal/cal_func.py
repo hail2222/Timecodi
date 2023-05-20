c8890928bdfd55d8efc60d9d7809d9958f48ed48
hyunyounglee99 <lisa1072@skku.edu>
2023-05-20 18:29:51 +0900

    구글캘린더 오류 수정

--- backend/app/googlecal/cal_func.py
+++ backend/app/googlecal/cal_func.py
@@ -1,6 +1,6 @@
 # 참고 사이트: https://developers.google.com/calendar/api/v3/reference/events/list?hl=ko#python
 from ..googlecal.cal import get_cal
-
+import os.path
 from googleapiclient.errors import HttpError
 from datetime import datetime
 
@@ -13,29 +13,40 @@ def get_event():
         now = datetime.now()
         now = str(now.date()) +'T00:00:00+09:00'
         print(now)
-        # print('Getting the upcoming 100 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
+        
 
-        if not events:
-            print('No upcoming events found.')
-            return
+        calendar_list = service.calendarList().list().execute()
+        for calendar_list_entry in calendar_list['items']:
+            # print(calendar_list_entry['id'])
+        # print('Getting the upcoming 100 events') 
+            if calendar_list_entry['id']=='ko.south_korea#holiday@group.v.calendar.google.com':
+                continue
+            events_result = service.events().list(calendarId=calendar_list_entry['id'], timeMin=now,
+                                                singleEvents=True,
+                                                orderBy='startTime').execute()
+            events = events_result.get('items', [])
 
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date')) # 시작날짜와 시간
-            end = event['end'].get('dateTime', event['end'].get('date')) # 종료날짜와 시간
-            start=start[:19]
-            end=end[:19]
+            if not events:
+                print('No upcoming events found.')
+                continue
 
-            # 기본설정: default/ 전체공개:public/ 비공개:private
-            try:
-                state=event['visibility']
-            except KeyError:
-                state='default'
+            for event in events:
+                start = event['start'].get('dateTime', event['start'].get('date')) # 시작날짜와 시간
+                end = event['end'].get('dateTime', event['end'].get('date')) # 종료날짜와 시간
+                start=start[:19]
+                end=end[:19]
+        
+                # 기본설정: default/ 전체공개:public/ 비공개:private
+                try:
+                    state=event['visibility']
+                except KeyError:
+                    state='default'
 
-            list_event.append([start,end,event['summary'],state]) 
+                list_event.append([start,end,event['summary'],state]) 
+        CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+        token_file=str(CURR_DIR)+'/token.json'
+        if os.path.exists(token_file):
+            os.remove(token_file)
         return list_event
 
     except HttpError as error:
