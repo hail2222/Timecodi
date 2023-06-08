e2085dddcda2b018d418b32ff884504ffca6b78d
SihyeonHong <hongsihyeon179@gmail.com>
2023-06-08 18:55:46 +0900

    delete personal event

--- frontend/src/app/my-timetable/components/CalendarModal.js
+++ frontend/src/app/my-timetable/components/CalendarModal.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 import axios from "axios";
 
 function CalendarModal({ date, openModal, setOpenModal, evtList }) {
-  let [updateEventContent, setUpdateEventContent] = useState({
+  const initEvt = {
     cid: "",
     cname: "",
     sdatetime: "",
@@ -12,13 +12,26 @@ function CalendarModal({ date, openModal, setOpenModal, evtList }) {
     visibility: "",
     weekly: "",
     enddate: "",
-  });
+  };
+  let [updateEventContent, setUpdateEventContent] = useState(initEvt);
+  //   let deleteall = false;
+  const [deleteall, setDeleteAll] = useState(false);
+
   const [showUpdateEvent, setShowUpdateEvent] = useState(false); // edit 버튼 눌렀을때 밑에 보이게 하는
 
-  const handleUpdateEvent = (cid) => {
+  const handleUpdateEvent = (e, cid) => {
     // edit 버튼 눌림
-    console.log(evtList); // 오늘의 모든 이벤트들이 들어있는 걸 확인할 수 있습니다
-    // cid에 해당하는 이벤트를 evtList에서 찾아서 updateEventContent에 저장해야 함
+    // console.log("e, cid", e, cid);
+    // console.log("CalendarModal", evtList);
+    // cid에 해당하는 이벤트를 evtList에서 찾아서 updateEventContent에 저장
+    if (evtList !== undefined) {
+      evtList.map((evt, index) => {
+        if (evt.cid === cid) {
+          console.log("evt", evt);
+          setUpdateEventContent(evt);
+        }
+      });
+    }
     setShowUpdateEvent(true);
   };
 
@@ -43,13 +56,12 @@ function CalendarModal({ date, openModal, setOpenModal, evtList }) {
        */
   };
   const deleteEvent = () => {
-    setShowUpdateEvent(false);
-
     // cid, deleteall(t/f)
-    /* 
+    const data = { cid: updateEventContent.cid, deleteall: deleteall };
+    console.log(data);
     axios
       .delete(
-        "https://port-0-timecodi-416cq2mlg8dr0qo.sel3.cloudtype.app/event",
+        `https://port-0-timecodi-416cq2mlg8dr0qo.sel3.cloudtype.app/event?cid=${data.cid}&deleteall=${data.deleteall}`,
         {
           headers: {
             Authorization: localStorage.getItem("token"),
@@ -59,12 +71,18 @@ function CalendarModal({ date, openModal, setOpenModal, evtList }) {
       .then((response) => {
         console.log(response.data);
         alert("Event Deleted.");
+        setUpdateEventContent(initEvt);
+        window.location.reload();
       })
       .catch((err) => {
         console.log(err);
+        alert("Event Delete Failed.");
       });
-       */
   };
+
+  useEffect(() => {
+    console.log("UpdateEventContent", updateEventContent);
+  }, [updateEventContent]);
 
   return (
     <Form className="card" style={{ width: "fit-content" }}>
@@ -114,12 +132,35 @@ function CalendarModal({ date, openModal, setOpenModal, evtList }) {
           <label>/my-timetable/timetable.js 참고 </label>
           <button
             type="button"
-            className="btn btn-secondary btn-sm"
+            className="btn btn-inverse-secondary btn-sm"
             onClick={() => {
               setShowUpdateEvent(false);
             }}
           >
             Close
+          </button>
+          <br></br>
+          <label className="form-check-label">
+            delete all repeated schedules?{"          "}
+            <input
+              type="checkbox"
+              className="form-check-input"
+              checked={deleteall}
+              onChange={() => {
+                setDeleteAll(!deleteall);
+              }}
+            />
+            <i className="input-helper"></i>{" "}
+          </label>
+
+          <button
+            type="button"
+            className="btn btn-inverse-danger btn-sm"
+            onClick={() => {
+              deleteEvent();
+            }}
+          >
+            DELETE
           </button>
         </Container>
       )}
@@ -136,7 +177,7 @@ function CalendarModal({ date, openModal, setOpenModal, evtList }) {
           <button
             type="button"
             className="btn btn-primary btn-sm"
-            onClick={(cid) => handleUpdateEvent(cid)}
+            onClick={(e) => handleUpdateEvent(e, cid)}
           >
             EDIT
           </button>
