73a28a70a8c1cd8efc5a4b2c0a110f3b9c602cf5
SihyeonHong <hongsihyeon179@gmail.com>
2023-10-22 13:38:28 +0900

    서버 주소 변경

--- frontend/src/app/apiurl.js
+++ frontend/src/app/apiurl.js
@@ -1,5 +1,4 @@
-const realURL =
-  "https://port-0-capstone-project-backend-12fhqa2llo0vfh0t.sel5.cloudtype.app";
+const realURL = "https://port-0-timecodi-jvpb2mlo0yal18.sel5.cloudtype.app";
 const localURL = "http://127.0.0.1:8000";
 const url = realURL;
 
