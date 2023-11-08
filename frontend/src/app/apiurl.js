8f7f21e512b0d299ad7156923c9b0a29bc46fdd1
SihyeonHong <hongsihyeon179@gmail.com>
2023-11-08 15:19:17 +0900

    서버 도메인 변경

--- frontend/src/app/apiurl.js
+++ frontend/src/app/apiurl.js
@@ -1,6 +1,7 @@
-const realURL = "https://port-0-timecodi-jvpb2mlo0yal18.sel5.cloudtype.app";
+const realURL = "https://port-0-timecodi-3yl7k2blopbyrq8.sel5.cloudtype.app/";
 const localURL = "http://127.0.0.1:8000";
 const url = realURL;
 
 // 이전 주소: https://port-0-timecodi-416cq2mlg8dr0qo.sel3.cloudtype.app
+// https://port-0-timecodi-jvpb2mlo0yal18.sel5.cloudtype.app
 export default url;
